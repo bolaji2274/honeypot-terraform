40e89b22d185dc9e519cfaed5f3097d63cfba141
Bolaji Hammed <hammedbolajihammed@gmail.com>
2023-07-30 14:34:51 +0100

    pfsense file added

--- honeypot-aws/pfsense.tf
+++ honeypot-aws/pfsense.tf
@@ -1,17 +1,17 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-# resource "aws_instance" "pfsense" {
-#   ami = "ami-0f1c68e571ab71af6"
-#   instance_type = "t3.medium"
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "pfsense" {
+  ami = "ami-0f1c68e571ab71af6"
+  instance_type = "t3.medium"
 
-#   tags = {
-#     Name = "pfsense"
-#   }
+  tags = {
+    Name = "Pfsense"
+  }
 
-#     root_block_device {
-#     volume_type           = "gp2"
-#     volume_size           = 30
-#     delete_on_termination = true
-#   }
-# }
+    root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 30
+    delete_on_termination = true
+  }
+}
